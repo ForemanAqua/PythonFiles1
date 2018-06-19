e46c7e0116df1775bae7913d975c35f3604b6fe0
ValeriyNeroznak <36760559+ValeriyNeroznak@users.noreply.github.com>
2018-06-19 13:08:02 +0300

    Update and rename 9''.py to 9 задание.py

--- Практика/Рівень B/9 задание.py
+++ Практика/Рівень B/9 задание.py
@@ -7,19 +7,10 @@ printTimeStamp("Valeriy Neroznak")
 
 
 a=input("Введіть номерний знак:")
-b=len(a)
-k=a[0:3].isdigit() and isinstance(a[4:6],str)and a[4:6].isupper()
-c=isinstance(a[0:2],str) and a[3:5].isdigit()  and a[0:3].isupper()
-if b==6:
-    print("Старіші номерні знаки")
-elif b==7:
-    print("Нові номерні знаки")
-else:
-    print("Введенні не правильні дані")
 
 if a[0:3].isdigit() and isinstance(a[4:6],str)and a[4:6].isupper():
     print("Старіші номерні знаки")
 elif isinstance(a[0:2],str) and a[3:5].isdigit()  and a[0:3].isupper():
     print("Нові номерні знаки")
 else:
-    print("Error!!!")
+    print("Помилка, введені неправильні дані")
