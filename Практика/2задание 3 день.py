0c9216b9b8e0dc59dbf7a371fd52cf77891ff2e3
ForemanAqua <36760559+ForemanAqua@users.noreply.github.com>
2018-06-20 12:03:57 +0300

    Create 2задание 3 день.py

---
+++ Практика/2задание 3 день.py
@@ -0,0 +1,35 @@
+import random
+lis1=[]
+def i():
+    for i in range(3):
+        q=random.randint(0,9)
+        w=random.randint(0,9)
+        e=random.randint(0,9)
+        r=random.randint(0,9)
+        t=random.randint(0,9)
+        y=random.randint(0,9)
+        u=random.randint(0,9)
+        i=random.randint(0,9)
+        o=random.randint(0,9)
+        lis=(str("+38(0"+str(q)+str(w)+")"+str(e)+str(r)+str(t)+"-"+str(y)+str(u)+"-"+str(i)+str(o)))
+        lis1.append((lis))
+    print(lis1)
+i()
+
+
+"""import random
+lis=[]
+def i():
+    for i in range(3):
+        q=random.randint(0,9)
+        w=random.randint(0,9)
+        e=random.randint(0,9)
+        r=random.randint(0,9)
+        t=random.randint(0,9)
+        y=random.randint(0,9)
+        u=random.randint(0,9)
+        i=random.randint(0,9)
+        o=random.randint(0,9)
+        lis=[str("+38(0"+str(q)+str(w)+")"+str(e)+str(r)+str(t)+"-"+str(y)+str(u)+"-"+str(i)+str(o))]
+        print(lis)
+i()"""
