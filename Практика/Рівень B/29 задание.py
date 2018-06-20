8e8c64e2322f6a43c4f29ce892330c9988756770
ForemanAqua <36760559+ForemanAqua@users.noreply.github.com>
2018-06-20 11:22:54 +0300

    Update 29 задание.py

--- Практика/Рівень B/29 задание.py
+++ Практика/Рівень B/29 задание.py
@@ -1,3 +1,10 @@
+import datetime
+
+def printTimeStamp(name):
+    print("Автор програми: " + name)
+    print("Час компіляції: " + str(datetime.datetime.now()),"\n")
+printTimeStamp("Valeriy Neroznak")
+
 """n=int(input())
 def fact(n):
     factors = []
