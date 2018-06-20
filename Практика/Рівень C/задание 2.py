7f85deb8cfefc1c0a2991702b2334889cbabda62
ForemanAqua <36760559+ForemanAqua@users.noreply.github.com>
2018-06-20 10:14:58 +0300

    Update задание 2.py

--- Практика/Рівень C/задание 2.py
+++ Практика/Рівень C/задание 2.py
@@ -1,8 +1,13 @@
 import datetime as d
 
-рік=int(input())
-місяць=int(input())
-день=int(input())
+def printTimeStamp(name):
+    print("Автор програми: " + name)
+    print("Час компіляції: " + str(d.datetime.now()),"\n")
+printTimeStamp("Valeriy Neroznak")
+
+рік=int(input("Введіть рік: "))
+місяць=int(input("Введіть місяць: "))
+день=int(input("Введіть день: "))
 дата=d.date(рік, місяць, день)
 f=дата+d.timedelta(days=1)
 
